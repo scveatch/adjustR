1b670314ffe81e70e5622824ea1253294bd740ba
scveatch <scveatch@willamette.edu>
2024-05-26 22:55:14 -0700

    Update tests / placement funcs

---
+++ R/primes.R
@@ -0,0 +1,67 @@
+#' Sieve of Eratosthenes
+#'
+#' Return a vector of prime numbers less than the given number using the
+#' sieve of Eratosthenes method
+#'
+#' @param n An integer specifying the upper limit (exclusive) for finding prime
+#' numbers.
+#' @returns A vector of prime numbers less than \code{n}.
+#' @details
+#' Throws an error if \code{n} is less than 2.
+#' @examples
+#' sieve(10)
+#' sieve(20)
+#' @export
+
+sieve <- function(n){
+  if (n < 1){
+    stop("Input must be greater than 0")
+  }
+  sieve <- rep(TRUE, n)
+  sieve[1] = FALSE
+  prime = 2
+
+  while (prime^2 <= n) {
+    if (sieve[prime]) {
+      # Ensure we only call seq.int when the range is valid
+      sieve[seq.int(prime^2, n, prime)] <- FALSE
+    }
+    # Find the next prime number
+    prime = prime + 1
+    while (prime <= n && !sieve[prime]) {
+      prime = prime + 1
+    }
+  }
+  return(which(sieve))
+}
+
+#' Next Prime Number
+#'
+#' Returns the next prime from a given number \code{n}.
+#'
+#' @param n A non-negative integer specifying the starting place for the
+#' next prime.
+#' @returns An integer value for the nearest prime number after \code{n}.
+#' @details
+#' Returns an error if \code{n} is negative.
+#'
+#' @examples
+#' next_prime(3)
+#' next_prime(25)
+#' @export
+
+next_prime <- function(n){
+  if (n <= 0){
+    stop("Input must be non-negative")
+  }
+  p = n + 1
+  vals = 2:n
+  while(TRUE){
+    if( !any( (p %% vals) == 0) ){
+      return(p)
+    }
+    p = p + 1
+  }
+}
+
+
---
+++ tests/testthat/test-primes.R
@@ -0,0 +1,22 @@
+test_that("Sieve works 5", {
+  expect_equal(sieve(5), c(2, 3, 5))
+})
+
+test_that("Sieve stops negative", {
+  expect_error(sieve(-1), "Input must be greater than 0")
+})
+
+test_that("Next Prime works 3", {
+  expect_equal(next_prime(3), 5)
+})
+
+test_that("Next Prime stops negative", {
+  expect_error(next_prime(-1), "Input must be non-negative")
+})
+
+test_that("Next Prime stops 0", {
+  expect_error(next_prime(0), "Input must be non-negative")
+})
+
+
+
